08fcc952c97a75f6ca8f7f5f85f5c0b23b1bf34b
Khoa Nguyen <nkhoa0101@yahoo.com>
2022-04-05 20:15:52 -0700

    Define metricSelector

--- src/Features/Weather/MetricGraphs.tsx
+++ src/Features/Weather/MetricGraphs.tsx
@@ -1,15 +1,53 @@
-import React, { FC } from 'react';
-// import {
-//   ApolloClient,
-//   ApolloProvider,
-//   useQuery,
-//   gql,
-//   InMemoryCache,
-// } from '@apollo/client';
-// import { useGeolocation } from 'react-use';
-// import LinearProgress from '@material-ui/core/LinearProgress';
-// import { Typography } from '@material-ui/core';
+import React, { FC, useState } from 'react';
+import {
+  ApolloClient,
+  ApolloProvider,
+  useQuery,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { Typography } from '@material-ui/core';
+import Chip from '../../components/Chip';
 
-const MetricGraphs: FC = () => (<div>This is the Metric Graphs</div>);
+const client = new ApolloClient({
+  uri: 'https://react-assessment.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+});
 
-export default MetricGraphs;
+const metricQuery = gql`
+  query{
+    getMetrics,
+  }
+`;
+
+type MetricsProps = {
+  metrics: Object;
+  setMetrics: any;
+};
+
+const MertricSelector: FC<MetricsProps> = (props) => {
+  console.log(props);
+  return (
+    <div>This is the MetricSelector</div>
+  );
+};
+
+const MetricGraphs: FC = () => {
+  const [metrics, setMetrics] = useState<Object>({});
+  const { loading, error, data } = useQuery(metricQuery);
+
+  if (loading) return <LinearProgress />;
+  if (error) return <Typography color="error">{error}</Typography>;
+  if (!data) return <Chip label="Metrics not found" />;
+
+  return (
+    <MertricSelector metrics={metrics} setMetrics={setMetrics} />
+  );
+};
+
+export default () => (
+  <ApolloProvider client={client}>
+    <MetricGraphs />
+  </ApolloProvider>
+);
